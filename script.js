9b22e606cd894c6b6dae43235c838f32e3d9bc3f
Junya Qiao <junyaqiao@gmail.com>
2021-09-11 12:49:50 -0400

    add all the choices and when make choices generate different pwd based on requirement

--- script.js
+++ script.js
@@ -2,15 +2,23 @@
 //check the functions, if checked reutrn true, not checked return false
 function checklowercase() {
   var checklower = document.getElementById("lowercase").checked;
+  console.log("lowerresult "+checklower);
+  return checklower;
 }
 function checkuppercase(){
   var checkupper = document.getElementById("uppercase").checked;
+  console.log("upperresult "+checkupper);
+  return checkupper;
 }
 function checknumeric(){
   var checknumeric = document.getElementById("numeric").checked;
+  console.log("numeric result "+checknumeric);
+  return checknumeric;
 }
 function checkspecial(){
   var checkspecial = document.getElementById("special").checked;
+  console.log("special result "+checkspecial);
+  return checkspecial;
 }
 // generate password function with only owercase
 function generatelowerPassword(length){
@@ -62,6 +70,178 @@ console.log("lower  "+generatelowerPassword(3));
 console.log("upper  "+generateupperPassword(3));
 console.log("numeric "+generatenumericPassword(3));
 console.log("special "+generatespecialPassword(3));
+checklowercase();
+
+function generatePassword(length){
+  //only lower
+if(checklowercase()===true && checkuppercase()===false && checknumeric()===false && checkspecial()===false){
+  var characters       = 'abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+  //only upper
+else if(checklowercase()==false && checkuppercase()==true && checknumeric()==false && checkspecial()==false){
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength = characters.length;
+  var result = '';
+    for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+
+//only number 
+else if(checklowercase()==false && checkuppercase()==false && checknumeric()==true && checkspecial()==false){
+  var characters = '1234567890';
+  var charactersLength = characters.length;
+  var result = '';
+    for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+
+//only special 
+else if(checklowercase()==false && checkuppercase()==false && checknumeric()==false && checkspecial()==true){
+  var characters = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result = '';
+    for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+
+//lower and upper
+else if(checklowercase()==true && checkuppercase()==true && checknumeric()==false && checkspecial()==false){
+  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+} 
+
+//lower and number 
+else if(checklowercase()==true && checkuppercase()==false && checknumeric()==true && checkspecial()==false){
+  var characters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result
+}
+
+//lower and special 
+else if (checklowercase()==true && checkuppercase()==false && checknumeric()==false && checkspecial()==true){
+  var characters = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+  r = characters.charAt(Math.floor(Math.random() * charactersLength));
+  result = result +r;
+ }
+ return result;
+}
+
+//upper and number
+else if (checklowercase()==false && checkuppercase()==true && checknumeric()==true && checkspecial()==false){
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  var charactersLength = characters.length;
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+  r = characters.charAt(Math.floor(Math.random() * charactersLength));
+  result = result +r;
+ }
+ return result;
+}
+//upper and special
+else if(checklowercase()==false && checkuppercase()==true && checknumeric()==false && checkspecial()==true){
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+  r = characters.charAt(Math.floor(Math.random() * charactersLength));
+  result = result +r;
+ }
+ return result;
+}
+
+//number and special 
+else if(checklowercase()==false && checkuppercase()==false && checknumeric()==true && checkspecial()==true){
+  var characters = '1234567890!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+  r = characters.charAt(Math.floor(Math.random() * charactersLength));
+  result = result +r;
+ }
+ return result;
+}
+
+//lower, upper and number
+else if(checklowercase()==true && checkuppercase()==true && checknumeric()==true && checkspecial()==false){
+  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+
+//lower, upper and special 
+else if(checklowercase()==true && checkuppercase()==true && checknumeric()==false && checkspecial()==true){
+  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+
+//upper number and special 
+else if(checklowercase()==false && checkuppercase()==true && checknumeric()==true && checkspecial()==true){
+  var characters = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+//lower, upper, numer and special
+else if(checklowercase()==true && checkuppercase()==true && checknumeric()==true && checkspecial()==true){
+  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var charactersLength = characters.length;
+  var result ='';
+  for ( var i = 0; i < length; i++ ) {
+    r = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result = result +r;
+ }
+ return result;
+}
+else{
+  var result ='please choose at least one type'
+  return result;
+}
+}
 
 
 // Get references to the #generate element
@@ -69,9 +249,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(9);
   var passwordText = document.querySelector("#password");
-
+  checklowercase();
+  checkuppercase();
+  checknumeric();
+  checkspecial();
   passwordText.value = password;
 
 }
